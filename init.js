df863a9d37d12a01a321b9d1e0b0d2e93d70d949
Suhwan Park <tnghks5977@gmail.com>
2020-02-24 16:18:49 +0900

    20-02-24

--- init.js
+++ init.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
